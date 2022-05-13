222716c326957d2bf71724c23d0784a9c16eea0f
Thomas Sashida Contrera <ths.contrera@gmail.com>
2022-05-13 11:54:57 -0300

    sorteia letra arrumado

--- Sorteia letra com restrições.py
+++ Sorteia letra com restrições.py
@@ -1,10 +1,21 @@
 import random
 
 def sorteia_letra(string,lista):
-
+    carac_esp = ['.', ',', '-', ';', ' ']
     lista_sorteavel = [] 
+    lista_restrita = []
+    
+    for letra in lista:
+        lista_restrita.append(letra)
+        if letra.lower() == letra:
+            lista_restrita.append(letra.upper())
+        else:
+            lista_restrita.append(letra.lower())
+
+    print(lista_restrita)
+        
     for caractere in string:
-        if caractere not in lista:
+        if caractere not in lista_restrita and caractere not in carac_esp:
             lista_sorteavel.append(caractere)
 
     if len(lista_sorteavel) == 0:
@@ -13,3 +24,5 @@ def sorteia_letra(string,lista):
     sorteio = random.choice(lista_sorteavel)
 
     return sorteio
+
+print(sorteia_letra('Andorra a-Velha',['a', 'r']))
